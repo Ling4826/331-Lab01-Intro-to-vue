32f065bd74745c160f3f14f5a07b18a57abcff4d
Ling4826 <143516833+Ling4826@users.noreply.github.com>
2026-07-06 13:17:48 +0700

    make saleMessage

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ createApp(({
     setup() {
         const product = ref('imon')
         const brand = ref('SE 331')
-        const image = ref('./assets/images/socks_blue.jpg')
         const link = ref('https://www.camt.cmu.ac.th./')
-        const inStock = ref(true)
         const inventory = ref(100)
         const OnSale = ref(true)
         const details = ref([
@@ -16,9 +14,13 @@ createApp(({
         ])
         const size = ref(['S', 'M', 'L'])
         const variants = ref([
-            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
         ])
+        const selectedVariant = ref(0)
+        function updateVariant(index) {
+            selectedVariant.value = index
+        }
         const cart = ref(0)
         function addToCart() {
             cart.value += 1
@@ -30,17 +32,30 @@ createApp(({
             image.value = variantImage
         }
         function updateinSale() {
-            if (inventory.value > 0) {
-                inventory.value = 0;
-                inStock.value = false;
-                OnSale.value = false;
+            if (variants.value[selectedVariant.value].quantity > 0) {
+                variants.value[selectedVariant.value].quantity = 0;
             }
             else {
-                inventory.value = 100;
-                inStock.value = true;
-                OnSale.value = true;
+                variants.value[selectedVariant.value].quantity = 100;
             }
         }
+        const image = computed(() => {
+            return variants.value[selectedVariant.value].image
+        })
+
+        const inStock = computed(() => {
+            return variants.value[selectedVariant.value].quantity > 0
+        })
+        const saleMessage = computed(() => {
+            if (inStock.value) {
+                return brand.value + ' ' + product.value + ' ' + variants.value[selectedVariant.value].color + ' is on sale'
+            }
+            else {
+                return brand.value + ' ' + product.value + ' ' + variants.value[selectedVariant.value].color + ' is not on sale'
+            }
+        })
+
+
 
         return {
             title,
@@ -54,8 +69,9 @@ createApp(({
             size,
             cart,
             addToCart,
-            updateImage,
-            updateinSale
+            updateVariant,
+            updateinSale,
+            saleMessage
 
         }
     }
