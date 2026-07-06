d67182a470ab269de0b91798d18307cad7c0ca66
Ling4826 <143516833+Ling4826@users.noreply.github.com>
2026-07-06 14:37:37 +0700

    12.3

--- components/ReviewForm.js
+++ components/ReviewForm.js
@@ -1,4 +1,4 @@
-const { reactive } = Vue
+const { reactive, toRefs } = Vue
 
 const reviewForm = {
     template:
@@ -6,13 +6,13 @@ const reviewForm = {
         `<form class="review-form" @submit.prevent="onSubmit">
         <h3>Leave a review</h3>
         <label for="name">Name:</label>
-        <input id="name" v-model="form.name">
+        <input id="name" v-model="name">
 
         <label for="review">Review:</label>
-        <textarea id="review" v-model="form.review"></textarea>
+        <textarea id="review" v-model="review"></textarea>
 
         <label for="rating">Rating:</label>
-        <select id="rating" v-model.number="form.rating">
+        <select id="rating" v-model.number="rating">
             <option>5</option>
             <option>4</option>
             <option>3</option>
@@ -21,7 +21,7 @@ const reviewForm = {
         </select>
 
         <label for="recommend">Would you recommend this product?</label>
-        <select id="recommend" v-model="form.recommend">
+        <select id="recommend" v-model="recommend">
             <option>Yes</option>
             <option>No</option>
         </select>
@@ -58,7 +58,7 @@ const reviewForm = {
         }
 
         return {
-            form,
+            ...toRefs(form),
             onSubmit
         }
     }
